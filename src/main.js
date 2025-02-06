cb8e6c9123614c1709cfcf8fccf86850d7296efa
SXH0212 <video19293@gmail.com>
2025-02-06 01:59:23 +0000

    過濾@vee-validate/rules的all規則

--- src/main.js
+++ src/main.js
@@ -6,6 +6,12 @@ import 'vue3-loading-overlay/dist/vue3-loading-overlay.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { currency, date } from '@/methods/filters';
 import httpMessageState from '@/methods/pushMessageState';
+import {
+  Form, Field, ErrorMessage, defineRule, configure,
+} from 'vee-validate';
+import * as AllRules from '@vee-validate/rules';
+import { localize, setLocale } from '@vee-validate/i18n';
+import zhTW from '@vee-validate/i18n/dist/locale/zh_TW.json';
 
 import App from './App.vue';
 import router from './router';
@@ -14,8 +20,26 @@ const app = createApp(App);
 app.config.globalProperties.$filters = {
   currency, date,
 };
+
+// 過濾掉 'all' 規則，並註冊其他規則
+Object.keys(AllRules).forEach((rule) => {
+  if (rule !== 'all') {
+    defineRule(rule, AllRules[rule]);
+  }
+});
+
+configure({
+  generateMessage: localize({ zh_TW: zhTW }), // 載入繁體中文語系
+  validateOnInput: true, // 當輸入任何內容直接進行驗證
+});
+// 設定預設語系
+setLocale('zh_TW');
+
 app.provide('httpMessageState', httpMessageState);//  前面是名稱，後面是 import 進來的 method
 app.use(VueAxios, axios);
 app.use(router);
 app.component('Loading', Loading);
+app.component('Form', Form);
+app.component('Field', Field);
+app.component('ErrorMessage', ErrorMessage);
 app.mount('#app');
--- src/views/UserCart.vue
+++ src/views/UserCart.vue
@@ -136,6 +136,81 @@
         </div>
       </div>
     </div>
+    <!-- 結帳表單 -->
+    <div class="my-5 row justify-content-center">
+      <Form class="col-md-6" v-slot="{ errors }" @submit="createOrder">
+        <div class="mb-3">
+          <label for="email" class="form-label">Email</label>
+          <Field
+            id="email"
+            name="email"
+            type="email"
+            class="form-control"
+            :class="{ 'is-invalid': errors['email'] }"
+            placeholder="請輸入 Email"
+            rules="email|required"
+            v-model="form.user.email"
+          ></Field>
+          <ErrorMessage name="email" class="invalid-feedback"></ErrorMessage>
+        </div>
+        <div class="mb-3">
+          <label for="name" class="form-label">收件人姓名</label>
+          <Field
+            id="name"
+            name="姓名"
+            type="text"
+            class="form-control"
+            :class="{ 'is-invalid': errors['姓名'] }"
+            placeholder="請輸入姓名"
+            rules="required"
+            v-model="form.user.name"
+          ></Field>
+          <ErrorMessage name="姓名" class="invalid-feedback"></ErrorMessage>
+        </div>
+        <div class="mb-3">
+          <label for="tel" class="form-label">收件人電話</label>
+          <Field
+            id="tel"
+            name="電話"
+            type="tel"
+            class="form-control"
+            :class="{ 'is-invalid': errors['電話'] }"
+            placeholder="請輸入電話"
+            rules="required"
+            v-model="form.user.tel"
+          ></Field>
+          <ErrorMessage name="電話" class="invalid-feedback"></ErrorMessage>
+        </div>
+        <div class="mb-3">
+          <label for="address" class="form-label">收件人地址</label>
+          <Field
+            id="address"
+            name="地址"
+            type="text"
+            class="form-control"
+            :class="{ 'is-invalid': errors['地址'] }"
+            placeholder="請輸入地址"
+            rules="required"
+            v-model="form.user.address"
+          ></Field>
+          <ErrorMessage name="地址" class="invalid-feedback"></ErrorMessage>
+        </div>
+        <div class="mb-3">
+          <label for="message" class="form-label">留言</label>
+          <textarea
+            name=""
+            id="message"
+            class="form-control"
+            cols="30"
+            rows="10"
+            v-model="form.message"
+          ></textarea>
+        </div>
+        <div class="text-end">
+          <button class="btn btn-danger">送出訂單</button>
+        </div>
+      </Form>
+    </div>
   </div>
 </template>
 
@@ -150,6 +225,15 @@ export default {
       },
       cart: {},
       coupon_code: '',
+      form: {
+        user: {
+          name: '',
+          email: '',
+          tel: '',
+          address: '',
+        },
+        message: '',
+      },
     };
   },
   inject: ['httpMessageState'],
@@ -211,6 +295,25 @@ export default {
         this.httpMessageState(response, '套用優惠券');
         this.getCart();
       });
+    },
+    removeCartItem(id) {
+      this.status.loadingItem = id;
+      const url = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/cart/${id}`;
+      this.isLoading = true;
+      this.$http.delete(url).then((response) => {
+        this.httpMessageState(response, '移除購物車品項');
+        this.status.loadingItem = '';
+        this.getCart();
+        this.isLoading = false;
+      });
+    },
+    createOrder() {
+      const url = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/order`;
+      const order = this.form;
+      this.$http.post(url, { data: order })
+        .then((res) => {
+          console.log(res);
+        });
     },
   },
   created() {
